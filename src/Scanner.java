ff40068f84f67c921cf171d14796fcb1d06ad559
LucasEmmanuelRM <lucas.emmanuel10@hotmail.com>
2024-06-25 01:28:38 -0300

    Adicionado \r como um separator

--- src/Scanner.java
+++ src/Scanner.java
@@ -176,6 +176,7 @@ public class Scanner {
 
     private boolean scanSeparator(){
         switch (charCorrente) {
+            case '\r':
             case '\n':
                 takeIt();
                 linhaCorrente++;
